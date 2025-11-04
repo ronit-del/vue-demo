d8ed59a074364fd542ae3b1b08897acceebc49d2
ronit.gosai <ronit.gosai@iflair.com>
2025-11-04 18:40:55 +0530

    auth changes

--- vue-app-backend/api/auth.js
+++ vue-app-backend/api/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 const db = require("../config/db");
 const pool = db.getPool();
 
